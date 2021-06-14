0314ed9c464d6efc7989148c05497847024e7b77
shungen <1941325247@qq.com>
2021-06-14 11:31:13 +0800

    添加测试项目StudentAnySystem

---
+++ StudentScoreAnySystem/src/TestMain.java
@@ -0,0 +1,10 @@
+/**
+ * @author 王顺根
+ * @version 1.0
+ * @date 2021/6/14 11:28
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("jajjaj");
+    }
+}
